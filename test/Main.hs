c264a13888d616b4c3d5b406b5c6d89110dfb1bc
nsv <nsv@valk.ws>
2019-03-14 20:07:53 +0100

    Add unit tests for tree equality.
    They should probably be more thorough, but - yknow.

--- test/Main.hs
+++ test/Main.hs
@@ -1,19 +1,38 @@
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified Data.Tree.Zipper as T
+import Data.Maybe ( fromJust )
+import Control.Monad ( (>=>) )
+
+import Data.Tree.Zipper (Tree, leaf, branch, root, down, up)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" 
-   [ unitTests ]
+   [ testGroup "Unit tests" [ treeUnitTests ] ]
 
-basicTree :: T.Tree () Int
-basicTree = T.branch () [T.leaf 1, T.branch () [T.leaf 2, T.leaf 3]]
+basicTree :: Tree () Char
+basicTree = branch () 
+   [ leaf 'A'
+   , branch () 
+      [ leaf 'B'
+      , leaf 'C' 
+      ]
+   ]
 
-unitTests :: TestTree
-unitTests = testGroup "Unit tests"
-  [ testCase "Equality" $
-      1 + 1 @?= (2 :: Int)
-  ]
+
+infix 1 @/=
+
+(@/=) :: (Eq a, Show a, HasCallStack) => a -> a -> Assertion
+x @/= y = x /= y @? "wrong equality detected:  " ++ show x ++ " and " ++ show y
+
+treeUnitTests :: TestTree
+treeUnitTests = testGroup "Trees"
+   [ testCase "Equality" 
+   $ basicTree @?= basicTree
+   , testCase "Inequality"
+   $ branch () [leaf 'A'] @/= branch () [leaf 'A',leaf 'B']
+   , testCase "Equality of nodes" 
+   $ (root basicTree) @?= ( fromJust . (down >=> up) $ root basicTree)  --(fromJust $ (down >=> up) . root basicTree)
+   ]
